3be4b582a7d2146106a05b314fabfc7e3ba6d8b0
Pivkina <ketrin_696@mail.ru>
2023-06-12 14:04:40 +0200

    fouth commit

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,13 @@ public class Main {
         CalcService service = new CalcService();
 
         System.out.println("10_000 / 20");
-        System.out.println(service.miles(10_000, 20));
+        System.out.println(service.miles(10_000));
 
         System.out.println("582 / 20");
-        System.out.println(service.miles(582, 20));
+        System.out.println(service.miles(582));
 
         System.out.println("25_865 / 20");
-        System.out.println(service.miles(25_865, 20));
+        System.out.println(service.miles(25_865));
 
     }
 }
